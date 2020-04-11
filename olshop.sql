1bdbceb31103578fb783a1d6f2641b49674f00f8
diahayulestari <62719853+diahayulestari@users.noreply.github.com>
2020-04-11 14:29:28 +0700

    Add files via upload

---
+++ olshop.sql
@@ -0,0 +1,213 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 09, 2020 at 02:48 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `olshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_detail_order`
+--
+
+CREATE TABLE `tbl_detail_order` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `order_id` int(10) DEFAULT NULL,
+  `produk` int(10) DEFAULT NULL,
+  `qty` int(10) DEFAULT NULL,
+  `harga` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama_kategori` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`id`, `nama_kategori`) VALUES
+(1, 'Pakaian'),
+(2, 'Sepatu'),
+(3, 'Topi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_order`
+--
+
+CREATE TABLE `tbl_order` (
+  `id` int(10) NOT NULL,
+  `tanggal` date DEFAULT NULL,
+  `pelanggan` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pelanggan`
+--
+
+CREATE TABLE `tbl_pelanggan` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `alamat` varchar(50) DEFAULT NULL,
+  `telp` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_produk`
+--
+
+CREATE TABLE `tbl_produk` (
+  `id_produk` int(10) UNSIGNED NOT NULL,
+  `nama_produk` varchar(50) DEFAULT NULL,
+  `deskripsi` varchar(50) DEFAULT NULL,
+  `harga` varchar(10) DEFAULT NULL,
+  `gambar` varchar(50) DEFAULT NULL,
+  `kategori` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_produk`
+--
+
+INSERT INTO `tbl_produk` (`id_produk`, `nama_produk`, `deskripsi`, `harga`, `gambar`, `kategori`) VALUES
+(1, 'Hoddie', 'Warna Hitam , Size L', '300000', 'add1.jpg', 1),
+(2, 'Kaos', 'Adidas 3 Foil, Size M', '150000', '1.jpg', 1),
+(3, 'Kemeja', 'Kemeja Lacoste Size L', '200000', '2.jpg', 1),
+(4, 'Adidas', 'Adidas Spesial, Size 40-42', '500000', 'adid.jpg', 2),
+(5, 'Reebok', 'Warna Putih, Size 39-42', '430000', 'reebok.jpg', 2),
+(6, 'Fila', 'Warna Putih, Size 38-40', '200000', 'fila.jpg', 2),
+(7, 'Topi Lacoste', 'Warna Abu-Abu', '100000', 'lacoste.jpg', 3),
+(8, 'Topo Lyle&Scoot', 'Warna Abu-Abu', '110000', 'lyle.jpg', 3),
+(9, 'Topi Polo', 'Warna Cream', '110000', 'polo.jpg', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `nama`, `email`, `username`, `password`) VALUES
+(0, 'diahayy', 'diahayy12@gmail.com', 'diahayy', '78d5b1858a0ba61efa15b74f9be1620c');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_detail_order`
+--
+ALTER TABLE `tbl_detail_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_pelanggan`
+--
+ALTER TABLE `tbl_pelanggan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  ADD PRIMARY KEY (`id_produk`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_detail_order`
+--
+ALTER TABLE `tbl_detail_order`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_pelanggan`
+--
+ALTER TABLE `tbl_pelanggan`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_produk`
+--
+ALTER TABLE `tbl_produk`
+  MODIFY `id_produk` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
